c5dd00548b05cdc9678f8e1d10922e7c77be65a4
taek0622 <min4644@gmail.com>
2024-06-03 01:47:40 +0900

    Feat: Add / routes
    - "/" 페이지 접속시 json 반환
    - 페이지 접속시 로그 남김

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 const webSocket = require("./socket");
 
+app.use((req, res, next) => {
+    next();
+    console.log(`A '${req.method}' request was received from '${req.url}'`);
+});
+
+app.get("/", (req, res, next) => {
+    res.json({
+        success: true,
+    });
+});
 
 const server = app.listen(port, () => {
     console.log(`Server is listening at localhost:${port}`);
