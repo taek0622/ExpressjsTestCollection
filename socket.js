80acee6af36a965f55167c4b953240704619fdc7
taek0622 <min4644@gmail.com>
2024-06-03 01:46:26 +0900

    Chore: Modify socket
    - 소켓 통신 수정

--- socket.js
+++ socket.js
@@ -5,16 +5,26 @@ module.exports = (server) => {
     wss.on("connection", (ws, req) => {
         const ip = req.headers["x-forward-for"] || req.connection.remoteAddress;
         console.log("새로운 클라이언트 접속", ip);
+
+        // 메시지 수신
         ws.on("message", (message) => {
-            console.log(JSON.parse(message));
+            const json = JSON.parse(message);
+            console.log(json);
+            ws.send(JSON.stringify(json));
         });
+
+        // 에러 발생
         ws.on("error", (error) => {
             console.log(error);
         });
+
+        // 연결 종료
         ws.on("close", () => {
             console.log("클라이언트 접속 해제", ip);
             clearInterval(ws.interval);
         });
+
+        // 지정된 시간마다 메시지 전송
         ws.interval = setInterval(() => {
             if (ws.readyState == ws.OPEN) {
                 ws.send("서버에서 클라이언트로 메시지를 보냅니다");
