278e0e4d5447a3ab6c54acc2fb8339f93463b809
taek0622 <min4644@gmail.com>
2024-06-22 03:01:35 +0900

    Feat: Add Login message
    - 새로운 유저 접속시 모든 유저에게 유저 접속 메시지 주는 기능 추가

--- socket.js
+++ socket.js
@@ -6,6 +6,18 @@ module.exports = (server) => {
     wss.on("connection", (ws, req) => {
         const ip = req.headers["x-forward-for"] || req.connection.remoteAddress;
         console.log("새로운 클라이언트 접속", ip);
+        const uuid = uuidv4();
+
+        wss.clients.forEach((client) => {
+            client.send(
+                JSON.stringify({
+                    messageID: uuid,
+                    userID: "SystemMessage",
+                    message: "새로운 유저가 접속했습니다.",
+                    date: "2024-11-01",
+                })
+            );
+        });
 
         // 메시지 수신
         ws.on("message", (message) => {
